24086eee2d98933a3c76dd09ad69fd1a266801c8
Vjacheslav Embaturov <portweinson@gmail.com>
2014-08-06 04:27:59 +0400

    helper headers added

---
+++ WordsGame/PWGShortcuts.h
@@ -0,0 +1,17 @@
+//
+//  PWGShortcuts.h
+//  WordsGame
+//
+//  Created by Vjacheslav Embaturov on 06.08.14.
+//  Copyright (c) 2014 Portweinson. All rights reserved.
+//
+
+#ifndef WordsGame_PWGShortcuts_h
+#define WordsGame_PWGShortcuts_h
+
+#define APP_DELEGATE ((PWGAppDelegate *)[UIApplication sharedApplication].delegate)
+#define USER_DEFAULTS [NSUserDefaults standardUserDefaults]
+
+#define DATA_MANAGER [PWGDataManager sharedInstance]
+
+#endif
---
+++ WordsGame/PWGUserDefaultKeys.h
@@ -0,0 +1,14 @@
+//
+//  PWGUserDefaultKeys.h
+//  WordsGame
+//
+//  Created by Vjacheslav Embaturov on 06.08.14.
+//  Copyright (c) 2014 Portweinson. All rights reserved.
+//
+
+#ifndef WordsGame_PWGUserDefaultKeys_h
+#define WordsGame_PWGUserDefaultKeys_h
+
+static NSString *const kUDKeyIsNotFirstAppLaunch = @"isNotFirstAppLaunch";
+
+#endif
