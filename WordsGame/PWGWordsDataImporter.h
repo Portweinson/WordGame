ae840a9da961ee935d1a6a519fd738b46ec33498
Vjacheslav Embaturov <portweinson@gmail.com>
2014-08-06 04:34:13 +0400

    PWGWordsDataImporter class added to project

---
+++ WordsGame/PWGWordsDataImporter.h
@@ -0,0 +1,13 @@
+//
+//  PWGWordsDataImporter.h
+//  WordsGame
+//
+//  Created by Vjacheslav Embaturov on 06.08.14.
+//  Copyright (c) 2014 Portweinson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PWGWordsDataImporter : NSObject
+
+@end
---
+++ WordsGame/PWGWordsDataImporter.m
@@ -0,0 +1,13 @@
+//
+//  PWGWordsDataImporter.m
+//  WordsGame
+//
+//  Created by Vjacheslav Embaturov on 06.08.14.
+//  Copyright (c) 2014 Portweinson. All rights reserved.
+//
+
+#import "PWGWordsDataImporter.h"
+
+@implementation PWGWordsDataImporter
+
+@end
