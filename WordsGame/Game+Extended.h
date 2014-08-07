bcf9c2d440f01c6708effaf6890792b585409636
Vjacheslav Embaturov <portweinson@gmail.com>
2014-08-07 15:23:48 +0400

    Game class category, game default name

---
+++ WordsGame/Game+Extended.h
@@ -0,0 +1,20 @@
+//
+//  Game+Extended.h
+//  WordsGame
+//
+//  Created by Vjacheslav Embaturov on 07.08.14.
+//  Copyright (c) 2014 Portweinson. All rights reserved.
+//
+
+#import "Game.h"
+
+extern NSUInteger const kGameNameLengthLimit;
+
+@interface Game (Extended)
+
+
+#pragma mark - Class methods
+
++ (NSString *)defaultGameName;
+
+@end
---
+++ WordsGame/Game+Extended.m
@@ -0,0 +1,20 @@
+//
+//  Game+Extended.m
+//  WordsGame
+//
+//  Created by Vjacheslav Embaturov on 07.08.14.
+//  Copyright (c) 2014 Portweinson. All rights reserved.
+//
+
+#import "Game+Extended.h"
+
+const NSUInteger kGameNameLengthLimit = 25;
+
+@implementation Game (Extended)
+
++ (NSString *)defaultGameName
+{
+    return NSLocalizedString(@"GAME Default name", nil);
+}
+
+@end
