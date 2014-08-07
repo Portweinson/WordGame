ed13ce47d8389aac4d455dd1571ec8b5b72bee36
Vjacheslav Embaturov <portweinson@gmail.com>
2014-08-07 04:39:36 +0400

    game setup ui added

--- WordsGame/PWGGameSetupViewController.m
+++ WordsGame/PWGGameSetupViewController.m
@@ -7,12 +7,22 @@
 //
 
 #import "PWGGameSetupViewController.h"
+#import "NSLocale+Extended.h"
+#import "PWGWordsManager.h"
 
-@interface PWGGameSetupViewController ()
+@interface PWGGameSetupViewController () <UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *barButtonDone;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldGameName;
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerGameLanguage;
+@property (weak, nonatomic) IBOutlet UILabel *labelWordsCount;
 
 @end
 
 @implementation PWGGameSetupViewController
+
+
+#pragma mark - View lifecycle
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,10 +39,89 @@
 	// Do any additional setup after loading the view.
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self selectDefaultPickerRow];
+    [self refreshWordsCountLabelText];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Actions
+
+- (IBAction)cancelButtonPressed:(UIBarButtonItem *)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)doneButtonPressed:(UIBarButtonItem *)sender
+{
+    
+}
+
+
+#pragma mark - Navigation
+
+
+
+#pragma mark -
+
+- (void)refreshWordsCountLabelText
+{
+    NSUInteger count = [WORDS_MANAGER wordsCountForLanguageCode:[self languageCodeForSelectedPickerRow]];
+    NSString *labelText = [NSString stringWithFormat:NSLocalizedString(@"GSVC LABEL Words count", nil), count];
+    self.labelWordsCount.text = labelText;
+}
+
+#pragma mark - UIPickerView methods
+
+- (void)selectDefaultPickerRow
+{
+    NSString *language = [NSLocale defaultLanguageCode];
+    NSUInteger index = [LANGUAGE_CODES indexOfObject:language];
+    [self.pickerGameLanguage selectRow:index inComponent:0 animated:NO];
+}
+
+- (NSString *)languageCodeForSelectedPickerRow
+{
+    return [LANGUAGE_CODES objectAtIndex:[self.pickerGameLanguage selectedRowInComponent:0]];
+}
+
+#pragma mark - UIPickerViewDelegate/UIPickerViewDataSource
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [LANGUAGE_CODES count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return [NSLocale localizedLanguageNameForLanguageCode:LANGUAGE_CODES[row]];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    [self refreshWordsCountLabelText];
+}
+
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
